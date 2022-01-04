1a6d711ddeee72b33bde9180d553be0785686e2b
Andrea <javascript@gmail.com>
2022-01-03 22:36:01 -0500

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
+import react from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//Componente de tipo funcional en react
+function Funcional(props) {
+  return (
+  <div>
+    <h1>{props.texto}</h1>
+    <p>{props.cont}</p>
+  </div>
+  );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Funcional texto="Mi titulo" cont="Parrafo"/>, document.getElementById("root"));
