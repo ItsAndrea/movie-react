25117cc88fab9762683f801e9b54b0f3ec1ee3d3
Andrea <javascript@gmail.com>
2022-01-04 14:43:57 -0500

    Componentes y styles

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { MoviesGrid } from "./MoviesGrid";
+
+export function App(){
+    return(
+        <div> 
+            <header>
+                <h1 className="title">Movies</h1>
+            </header>
+            <main>
+                <MoviesGrid />
+            </main>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
-import react from 'react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { App } from "./App"
 
-//Componente de tipo funcional en react
-function Funcional(props) {
-  return (
-  <div>
-    <h1>{props.texto}</h1>
-    <p>{props.cont}</p>
-  </div>
-  );
-}
-
-ReactDOM.render(<Funcional texto="Mi titulo" cont="Parrafo"/>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
